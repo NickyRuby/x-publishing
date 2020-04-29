499dd1d461fcc960fd95cc3949507ee4c0d65c2c
NickyRuby <nikita.lobyntsev@gmail.com>
2020-04-29 08:06:32 +0300

    Make rounters and db migrations

--- sever.js
+++ sever.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 const errorHandler = require('error-handler');
 const morgan = require('morgan');
 const express = require('express');
+const apiRouter = require('./api/api.js');
 
 const app = express();
-
 const PORT = process.env.PORT || 4001;
+
 app.use(bodyParser.json());
 app.use(cors);
 app.use(morgan('tiny'));
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
     console.log('Im listening');
